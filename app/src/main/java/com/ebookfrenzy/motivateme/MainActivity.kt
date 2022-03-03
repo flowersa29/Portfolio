c63b7c2b16bcbc9ad839fd140cb462f7ef09eed6
Flowersa29 <flowersa29@gmail.com>
2022-03-02 20:48:45 -0600

    Added hide keyboard

--- app/src/main/java/com/ebookfrenzy/motivateme/MainActivity.kt
+++ app/src/main/java/com/ebookfrenzy/motivateme/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.ebookfrenzy.motivateme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -32,10 +34,31 @@ class MainActivity : AppCompatActivity() {
             //Get the username from the editText
             val username = name.text
 
-            //Update the textView to display our message
-            message.text = "Hello $username"
+            // pick a motivational message
+            val motivationalMessage = listOf("Keep working hard", "Never give in!", "Keep your head up","You can do it!", "You cant stop me!")
+            val index = (0..4).random()
+            val currentMessage = motivationalMessage[index]
 
-            //Clear the editText and use setText because
+        //Update the textView to display our message
+            if (username.toString() == "") {
+                message.text = "Make sure to enter your name"
+            } else {
+                message.text = "Hello $username! $currentMessage"
+            }
+
+            //Clear the editText and use setText because editText's require an editable
+            name.setText("")
+
+            //Hide the keyboard
+            hideKeyboard()
+
         }
+
+    // Hide the keyboard using an InputMethodManager
+    private fun hideKeyboard() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(name.windowToken, 0)
+
+    }
 
     }
